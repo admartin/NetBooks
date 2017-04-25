5385b6b774c7386133135519b1b50144a3ecf609
Abigail Martin <admartin@users.noreply.github.com>
2017-04-25 17:57:56 -0400

    Can use as model for other create statements

--- src/netbooks/persistlayer/ReviewPersistImpl.java
+++ src/netbooks/persistlayer/ReviewPersistImpl.java
@@ -1,12 +1,17 @@
 package netbooks.persist;
 
 import netbooks.objectlayer.Review;
+import netbooks.objectlayer.User;
+
+import java.util.List;
 
 public class ReviewPersistImpl {
 
-	public static void createReview(Review review) {
-		// TODO Auto-generated method stub
-		
+	public static void createReview(int id, int rating, String details, int bookID, String username)
+	{
+		User user = UserLogicImpl.getFullUserInfo(username).get(0);
+		Review review = new Review(-1, rating, details, bookID, user);
+		ReviewPersistImpl.createReview(review);
 	}
-
+	
 }
