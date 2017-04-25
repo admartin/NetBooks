d0bfe6f7e737a4823d75c98df3b8dcb0176e1fdb
ndesai94 <ndesai94@uga.edu>
2017-04-25 16:30:06 -0400

    persist

---
+++ src/netbooks/persistlayer/ReviewPersistImpl.java
@@ -0,0 +1,12 @@
+package netbooks.persist;
+
+import netbooks.objectlayer.Review;
+
+public class ReviewPersistImpl {
+
+	public static void createReview(Review review) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
