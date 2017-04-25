3c1f12eeb62db0b0f9ba381801339ce5fb08ee32
ndesai94 <ndesai94@uga.edu>
2017-04-25 16:30:48 -0400

    last persist class

---
+++ src/netbooks/persistlayer/UserPersistImpl.java
@@ -0,0 +1,49 @@
+package netbooks.persist;
+
+import java.util.List;
+
+import netbooks.objectlayer.User;
+
+public class UserPersistImpl {
+
+	public static List<User> getUserForLogin(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<User> getFullUserInfo(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void createUser(netbooks.objectlayer.User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void updateEmail(String username, String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void updatePassword(String username, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void updateAddress(String username, String street, String city, String state, int zip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void updateSub(String username, int sub) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addWaitlistEntry(String username, String title) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
