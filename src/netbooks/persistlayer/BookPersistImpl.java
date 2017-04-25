8f9be7f21ae8eff49e6f81b909eaf204d5210394
ndesai94 <ndesai94@uga.edu>
2017-04-25 16:29:21 -0400

    Add files via upload

---
+++ src/netbooks/persistlayer/BookPersistImpl.java
@@ -0,0 +1,34 @@
+package netbooks.persist;
+
+import java.util.List;
+
+import netbooks.objectlayer.Book;
+
+public class BookPersistImpl {
+
+	public static List<Book> getBooksByGenre(String genre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<Book> getBooksByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<Book> getBooksByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void checkOutBook(String username) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static List<Book> getWaitlist(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
