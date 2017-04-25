ed86e365b4b979976019aff241a47845bc8bbde7
Abigail Martin <admartin@users.noreply.github.com>
2017-04-25 18:52:09 -0400

    create user done

--- src/netbooks/persistlayer/UserPersistImpl.java
+++ src/netbooks/persistlayer/UserPersistImpl.java
@@ -1,10 +1,16 @@
 package netbooks.persist;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 import netbooks.objectlayer.User;
 
 public class UserPersistImpl {
+	
+	public static Connection conn;
 
 	public static List<User> getUserForLogin(String username) {
 		// TODO Auto-generated method stub
@@ -16,8 +22,43 @@ public class UserPersistImpl {
 		return null;
 	}
 
-	public static void createUser(netbooks.objectlayer.User user) {
-		// TODO Auto-generated method stub
+	public static void createUser(User user) {
+		String insertSql = "insert into netbooks.users (username, password, email, fname, lname, birthdate, address, city, state, zipcode, subscription) values (?,?,?,?,?,?,?,?,?,?,?)";            
+		PreparedStatement stmt;
+
+		try {
+			stmt = (PreparedStatement) conn.prepareStatement(insertSql);
+
+			if( user.getUsername() != null)
+				stmt.setString(1, user.getUsername());
+			if(user.getPassword() != null )
+				stmt.setString(2, user.getPassword());
+			if(user.getEmail() != null)
+				stmt.setString(3, user.getEmail());
+			if(user.getFname() != null)
+				stmt.setString(4, user.getFname());
+			if(user.getLname() != null)
+				stmt.setString(5, user.getLname());
+			if(user.getBirthdate() != null)
+				stmt.setString(6, user.getBirthdate());
+			if(user.getAddress() != null)
+				stmt.setString(7, user.getAddress());
+			if(user.getCity() != null)
+				stmt.setString(8, user.getCity());
+			if(user.getState() != null)
+				stmt.setString(9, user.getState());
+			if(user.getZipcode() != -1)
+				stmt.setInt(10, user.getZipcode());
+			if(user.getSubscription() != -1)
+				stmt.setInt(11, user.getSubscription());
+			
+			stmt.executeUpdate();
+
+		}
+		catch( SQLException e ) {
+			e.printStackTrace();
+			System.out.println( "User.save: failed to save a User: " + e );
+		}
 		
 	}
 
