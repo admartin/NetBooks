e98549207bdc99cfb7c33e7c3b436ac7a1997d9b
Abigail Martin <admartin@users.noreply.github.com>
2017-04-25 23:43:49 -0400

    Getting closer

--- src/netbooks/boundary/SignInServlet.java
+++ src/netbooks/boundary/SignInServlet.java
@@ -14,7 +14,9 @@ import javax.servlet.http.HttpSession;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapperBuilder;
 import freemarker.template.SimpleHash;
+import netbooks.logiclayer.BookLogicImpl;
 import netbooks.logiclayer.UserLogicImpl;
+import netbooks.objectlayer.Book;
 import netbooks.objectlayer.User;
 
 /**
@@ -55,7 +57,6 @@ public class SignInServlet extends HttpServlet {
 	}
 	
 	private void validateLogin(HttpServletRequest request, HttpServletResponse response){
-		boolean pass = false;
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
@@ -65,29 +66,21 @@ public class SignInServlet extends HttpServlet {
 				sess.invalidate();
 				sess = request.getSession();
 			}//create new session if a user was logged in prior
-			synchronized(sess){
-			//check if pass is valid
 			List<User> ulist = UserLogicImpl.getUserForLogin(username);	
-			if(ulist.size() != 1){
-				//username doesn't exist, err
-			}
-			else{
-				
-			}
-			/*
-			 * if returned userlist from getUser sql call returned a user 
-			 * and if returned password from userlist equals the provided password
-			 * then everything is okay
-            if(!((userlist.size() < 1)&&(password.equals(userlist.get(0).getPassword()) == false)))
-            {   pass = true;
-            	sesh.setAttribute("username", userlist.get(0).getUsername());
-            	if(userlist.get(0).getSubscription() == 0)
+
+			 //if returned userlist from getUser sql call returned a user 
+			 //and if returned password from userlist equals the provided password
+			 //then everything is okay
+            if(!((ulist.size() < 1)&&(password.equals(ulist.get(0).getPassword()) == false)))
+            {   
+            	sess.setAttribute("username", ulist.get(0).getUsername());
+            	if(ulist.get(0).getSubscription() == 0)
             		sess.setAttribute("premium", false);
             	else
             		sess.setAttribute("premium", true);
+            	showHomePage(request, response);
             }
-			 */
-			}
+			
 			
 		}
 		catch(Exception e){
@@ -100,28 +93,29 @@ public class SignInServlet extends HttpServlet {
 		DefaultObjectWrapperBuilder db = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);
 		SimpleHash root = new SimpleHash(db.build());
 		String templateName = "home.ftl";
+		
 		HttpSession sess = request.getSession();
 		
-		//root.put("username", sess.getAttribute("username") );
+		root.put("username", sess.getAttribute("username") );
 
 		//get books by genre -scifi
-			//List<Books> scifi = BookLogicImpl.selectBooksByGenre("Sci-Fi");
-			//root.put("scifi", scifi );
+			List<Book> scifi = BookLogicImpl.getBooksByGenre("Sci-Fi");
+			root.put("scifi", scifi );
 		//get books by genre -adventure
-			//List<Books> adven = BookLogicImpl.selectBooksByGenre("Adventure");
-			//root.put("adven", adven );
+			List<Book> adven = BookLogicImpl.getBooksByGenre("Adventure");
+			root.put("adven", adven );
 		//get books by genre -drama
-			//List<Books> drama = BookLogicImpl.selectBooksByGenre("Drama");
-			//root.put("drama", drama );
+			List<Book> drama = BookLogicImpl.getBooksByGenre("Drama");
+			root.put("drama", drama );
 		//get books by genre -horror
-			//List<Books> adven = BookLogicImpl.selectBooksByGenre("Horror");
-			//root.put("horror", horror );
+			List<Book> horror = BookLogicImpl.getBooksByGenre("Horror");
+			root.put("horror", horror );
 		//get books by genre -romance
-			//List<Books> adven = BookLogicImpl.selectBooksByGenre("Romance");
-			//root.put("romance", romance);
+			List<Book> romance = BookLogicImpl.getBooksByGenre("Romance");
+			root.put("romance", romance);
 		
 		//place variable detailing whether or not a user has premium
-		//root.put("premium", sess.getAttribute("premium") );
+		root.put("premium", sess.getAttribute("premium") );
 		
 		processor.runTemp(templateName, root, request, response);
 	}
