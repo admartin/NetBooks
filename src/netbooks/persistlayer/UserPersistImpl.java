976e354632c574277c4d4222e58a01f2ea89b137
Abigail Martin <admartin@users.noreply.github.com>
2017-04-26 16:04:25 -0400

    Should be fixed?

--- src/netbooks/persistlayer/UserPersistImpl.java
+++ src/netbooks/persistlayer/UserPersistImpl.java
@@ -1,125 +1,117 @@
-package netbooks.persistlayer;
+package netbooks.persist;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+
 import com.mysql.jdbc.PreparedStatement;
 
+import netbooks.objectlayer.Book;
 import netbooks.objectlayer.User;
 
 public class UserPersistImpl {
-	
+
 	public static Connection conn = null;
 
 	public static List<User> getUserForLogin(String username) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
 
-	    List<User> userList = new ArrayList<User>();
-	    
-	    String sql = "SELECT u.username, u.password, u.subscription FROM users u WHERE u.username = ?";
-	    PreparedStatement stmt = null;
-	    
-	    StringBuffer query = new StringBuffer(100);
-	    StringBuffer condition = new StringBuffer(100);
-	    
-	    condition.setLength(0);
-	    
-	    query.append(sql);
-	    
-	    try {
-		stmt = (PreparedStatement) conn.prepareStatement(sql);
-		stmt.setString(1, username);                          //may be different index other than 1
-		int assign = stmt.executeUpdate();
-		if(assign != -1) {
-		    ResultSet rs = stmt.getResultSet();
-		    String user = rs.getString(1);
-		    String pass = rs.getString(2);
-		    int subscription = rs.getInt(3);
-		    
-		    User user1 = new User(user, pass, subscription);
-		    userList.add(user1);
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-	    } catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	    
-	    return userList;
+
+		List<User> userList = new ArrayList<User>();
+
+		String sql = "SELECT u.username, u.password, u.subscription FROM users u WHERE u.username = ?";
+		PreparedStatement stmt;
+
+		try {
+			stmt = (PreparedStatement) conn.prepareStatement(sql);
+			stmt.setString(1, username);                         
+			stmt.executeQuery();
+			ResultSet rs = stmt.getResultSet();
+			
+			while( rs.next() ) {
+				String user = rs.getString(1);
+				String pass = rs.getString(2);
+				int subscription = rs.getInt(3);
+
+				User builder = new User().buildString(user).buildPassword(pass).buildSub(subscription).build();
+				userList.add(builder);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return userList;
 
 
 	}
 
 	public static List<User> getFullUserInfo(String username) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
-	    
-	    List<User> userList = new ArrayList<User>();
-	    Book booksCheckedOut[] = null;                       //not sure how to retrieve these books
-	    Book booksRead[] = null;
-	    
-	    
-	    String sql = "SELECT * FROM users u WHERE u.username = ?";
-	    PreparedStatement stmt = null;
-	    
-	    StringBuffer query = new StringBuffer(100);
-	    StringBuffer condition = new StringBuffer(100);
-	    
-	    condition.setLength(0);
-	    
-	    query.append(sql);
-	    
-	    try {
-		stmt = (PreparedStatement) conn.prepareStatement(sql);
-		stmt.setString(1, username);                               //may be other index other than 1
-		int assign = stmt.executeUpdate();
-		if(assign != -1) {
-		    ResultSet rs = stmt.getResultSet();
-		    int id = rs.getInt(1);
-		    String user = rs.getString(2);
-		    String password = rs.getString(3);
-		    String fname = rs.getString(4);
-		    String lname = rs.getString(5); //some of these are probably off, I don't think my 
-		    String email = rs.getString(6);//database is up to date with correct columns
-		    String birthdate = rs.getString(7);
-		    String address = rs.getString(8);
-		    String city = rs.getString(9);
-		    String state = rs.getString(10);
-		    int zipcode = rs.getInt(11);
-		    int cardNum = rs.getInt(12);
-		    int subscription = rs.getInt(13);
-		    
-		    User user1 = new User(id, username, password, fname, lname, birthdate, address, city, state, zipcode, cardNum, subscription, booksCheckedOut, booksRead, email);
-		    userList.add(user1);
+
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-	    } catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	    
-	    return userList;
+
+		List<User> userList = new ArrayList<User>();
+		Book booksCheckedOut[] = null;                       
+		Book booksRead[] = null;
+
+		String sql = "SELECT * FROM users u WHERE u.username = ?";
+		PreparedStatement stmt;
+
+		try {
+			stmt = (PreparedStatement) conn.prepareStatement(sql);
+			stmt.setString(1, username);      
+
+			stmt.executeQuery();
+			ResultSet rs = stmt.getResultSet();
+
+			while( rs.next() ) {
+				int id = rs.getInt(1);
+				String user = rs.getString(2);
+				String password = rs.getString(3);
+				String fname = rs.getString(4);
+				String lname = rs.getString(5); //some of these are probably off, I don't think my 
+				String email = rs.getString(6);//database is up to date with correct columns
+				String birthdate = rs.getString(7);
+				String address = rs.getString(8);
+				String city = rs.getString(9);
+				String state = rs.getString(10);
+				int zipcode = rs.getInt(11);
+				int cardNum = rs.getInt(12);
+				int subscription = rs.getInt(13);
+
+				User user1 = new User(id, username, password, fname, lname, birthdate, address, city, state, zipcode, cardNum, subscription, booksCheckedOut, booksRead, email);
+				userList.add(user1);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return userList;
 	}
 
 	public static void createUser(User user) {
 
-			try {
-				conn = DbUtils.connect();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		String insertSql = "insert into netbooks.users (username, password, email, fname, lname, birthdate, address, city, state, zipcode, subscription) values (?,?,?,?,?,?,?,?,?,?,?)";            
 		PreparedStatement stmt;
 
@@ -148,9 +140,7 @@ public class UserPersistImpl {
 				stmt.setInt(10, user.getZipcode());
 			if(user.getSubscription() != -1)
 				stmt.setInt(11, user.getSubscription());
-			
-			System.out.println(stmt.toString());
-			
+
 			stmt.executeUpdate();
 
 		}
@@ -158,132 +148,133 @@ public class UserPersistImpl {
 			e.printStackTrace();
 			System.out.println( "User.save: failed to save a User: " + e );
 		}
-		
+
 	}
 
 	public static void updateEmail(String username, String email) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
 
-	    String updateSql = "UPDATE users SET email = ? WHERE username = ?";
-	    PreparedStatement stmt2;
-	    
-	    try {
-		stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
-		
-		stmt2.setString(3, email);               //may be different index other than 3
-		
-		stmt2.executeUpdate();
-	    } catch(SQLException e) {
-		e.printStackTrace();
-		System.out.println( "Could not update email at this time.\nError:\t" + e );
-	    }
-	    
-		
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String updateSql = "UPDATE users SET email = ? WHERE username = ?";
+		PreparedStatement stmt2;
+
+		try {
+			stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
+			stmt2.setString(1, email);    
+			stmt2.setString(2, username);               //may be different index other than 3
+
+			stmt2.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println( "Could not update email at this time.\nError:\t" + e );
+		}
+
+
 	}
 
 	public static void updatePassword(String username, String password) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
 
-	    String updateSql = "UPDATE users SET password = ? WHERE username = ?";
-	    PreparedStatement stmt2;
-	    
-	    try {
-		stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
-		
-		stmt2.setString(2, password);               //may be different index other than 2
-		
-		stmt2.executeUpdate();
-	    } catch(SQLException e) {
-		e.printStackTrace();
-		System.out.println( "Could not update password at this time.\nError:\t" + e );
-	    }
-	    
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String updateSql = "UPDATE users SET password = ? WHERE username = ?";
+		PreparedStatement stmt2;
+
+		try {
+			stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
+			stmt2.setString(2, username);    
+			stmt2.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println( "Could not update password at this time.\nError:\t" + e );
+		}
+
 	}
 
 	public static void updateAddress(String username, String street, String city, String state, int zip) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
 
-	    String updateSql = "UPDATE users SET address = ?, city = ?, state = ?, zipcode = ? WHERE username = ?";
-	    PreparedStatement stmt2;
-	    
-	    try {
-		stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
-		
-		stmt2.setString(7, street);
-		stmt2.setString(9, city);              //these indeces may be wrong
-		stmt2.setString(10, state);
-		stmt2.setInt(8, zip);
-		
-		stmt2.executeUpdate();
-	    } catch(SQLException e) {
-		e.printStackTrace();
-		System.out.println( "Could not update address at this time.\nError:\t" + e );
-	    }
-	    
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String updateSql = "UPDATE users SET address = ?, city = ?, state = ?, zipcode = ? WHERE username = ?";
+		PreparedStatement stmt2;
+
+		try {
+			stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
+
+			stmt2.setString(1, street);
+			stmt2.setString(2, city);              //these indeces may be wrong
+			stmt2.setString(3, state);
+			stmt2.setInt(4, zip);
+			stmt2.setString(5, username);    
+			stmt2.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println( "Could not update address at this time.\nError:\t" + e );
+		}
+
 	}
 
 	public static void updateSub(String username, int sub) {
-	    
-	    try {
-		conn = DbUtils.connect();
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
 
-	    String updateSql = "UPDATE users SET subscription = ? WHERE username = ?";
-	    PreparedStatement stmt2;
-	    
-	    try {
-		stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
-		
-		stmt2.setInt(11, sub);               //may be different index other than 11
-		
-		stmt2.executeUpdate();
-	    } catch(SQLException e) {
-		e.printStackTrace();
-		System.out.println( "Could not update subscription at this time.\nError:\t" + e );
-	    }
-	    
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String updateSql = "UPDATE users SET subscription = ? WHERE username = ?";
+		PreparedStatement stmt2;
+
+		try {
+			stmt2 = (PreparedStatement) conn.prepareStatement(updateSql);
+			stmt2.setInt(1, sub);
+			stmt2.setString(2, username);    
+			stmt2.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println( "Could not update subscription at this time.\nError:\t" + e );
+		}
+
 	}
 
-    public static void addWaitlistEntry(String username, int bookID) {   //changed from book title to id to match waitlist table
+	public static void addWaitlistEntry(String username, int bookID) {   //changed from book title to id to match waitlist table
 
-	try {
-	    conn = DbUtils.connect();
-	} catch (Exception e) {
-	    e.printStackTrace();
+		try {
+			conn = DbUtils.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String insertSql = "INSERT INTO waitlist (Users_username, Books_id) VALUES (?, ?)";
+		PreparedStatement stmt2;
+
+		try {
+			stmt2 = (PreparedStatement) conn.prepareStatement(insertSql);
+
+			stmt2.setString(1, username);               //may be different indeces
+			stmt2.setInt(2, bookID);
+
+			stmt2.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println( "Could not add waitlist entry at this time.\nError:\t" + e );
+		}
+
 	}
-	
-	String insertSql = "INSERT INTO waitlist (id, Users_username, Books_id) VALUES (?, ?, ?)";
-	PreparedStatement stmt2;
-	
-	try {
-	    stmt2 = (PreparedStatement) conn.prepareStatement(insertSql);
-	    
-	    stmt2.setString(2, username);               //may be different indeces
-	    stmt2.setInt(3, bookID);
-	    
-	    stmt2.executeUpdate();
-	} catch(SQLException e) {
-	    e.printStackTrace();
-	    System.out.println( "Could not add waitlist entry at this time.\nError:\t" + e );
-	}
-	
-    }
 
 }
